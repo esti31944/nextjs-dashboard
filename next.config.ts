8f701e5f4e8f29f549deab92299d32bfdaaf7595
esti31944 <esti31944@gmail.com>
2025-10-26 09:52:55 +0200

    continue chapters 11

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   cacheComponents: {
+//     ppr: 'incremental'
+//   }
+// };
+const nextConfig = {
+  experimental: {
+    cacheComponents: true,
+  },
 };
 
 export default nextConfig;
